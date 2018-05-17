4e2d7a60731f2fb93d2732b2e187487c7c7b012f
Armando Carvajal <armandoecr_96@hotmail.com>
2018-05-17 12:00:39 -0500

    data.js

---
+++ README.md
@@ -0,0 +1,104 @@
+#Data
+
+It is a package dedicated to the easy extraction of data from commercial pages of facebook.
+
+##Features
+- Basic information on company Facebook pages
+- Gallery on company Facebook pages
+- It only works for the English and Spanish versions of Facebook
+
+##Installation
+
+First download the npm library of data:
+``` bash
+npm install data.js --save
+```
+
+##Usage
+
+```js
+const data = requiere('./data')
+
+//Receive the set of images from the facebook page
+data.gallery(facebookURL)
+	.then(res => console.log(res))
+    .catch(console.err)
+    
+//Reveive the set of basic information from the facebook page
+data.information(facebookURL)
+	.then(res => console.log(res))
+    .catch(console.err)
+```
+
+- `facebookURL` is the url taken from the start of the facebook business page to evaluate
+
+###Return
+
+This is the way to recive the information from information function
+```json
+{ 
+	bussinesInformation:
+	{
+		name: StringName,
+		logo: logoURL,
+		payment: [ credit card acept],
+		phone: CellPhone,
+		description: Desrcription 
+	},
+	ubication:
+	{
+		latitude: Latitude,
+		longitude: longitude,
+		address: Address,
+		city: City 
+	},
+	categories: 
+	{ 
+		categories: Category,
+		subcategories: Subcategory 
+	},
+	schedule: 
+	{ 
+		hour: [open - close],
+		days: [days] 
+	} 
+}
+```
+- `credit card acept` It is the set of types of cards accepted in the company
+- `open-close` It is the set of all available schedules provided by the facebook page.
+
+- `days` It is the set of all available days provided by the facebook page
+- The set of schedules corresponds to the set of days
+
+**Note:** There is an error that makes the information of hours and days of the week not always available. 
+
+This is the way to recive the information from gallery function
+```json
+  var jsonGal = 
+ { gallery:   
+ 	[ 	 
+     { img: URLimage },
+     { img: URLimage },
+     { img: URLimage },
+     { img: URLimage },
+     { img: URLimage }
+     ]
+  }
+```
+For the individual view of each one of url obtained from the gallery we use the following function
+
+```js
+data.gallery(facebookURL).then(res => console.log(res.gallery[0].img))
+```
+
+The information that does not exist on the facebook page will be represented as a String without characters. It is represented in this way: 
+
+```json
+categories: 
+{ 
+	categories: '',
+	subcategories: '', 
+}
+```
+
+
--- data.js
+++ data.js
@@ -1,39 +1,164 @@
 const puppeteer = require('puppeteer');
-const translate = require('translate');
 
-(async () => {
-
-    //var facebookPage = document.getElementById("facebookPage").value;
-    //console.log(facebookPage)
+exports.information = async function information(facebookPage) {
     const browser = await puppeteer.launch({
-        headless: false
+        headless: true
     });
+
     const page = await browser.newPage();
-    
-    await page.goto('https://www.facebook.com/AluxPlayaDelCarmen/');
     await page.waitFor(1000);
 
-    await page.goto('https://www.facebook.com/pg/AluxPlayaDelCarmen/about/?ref=page_internal');
-    //await page.click('#u_0_g > div:nth-child(3) > a')
+    await page.goto(facebookPage + 'about/?ref=page_internal');
+    try {
+        await page.click('#u_0_o > div._4bl9 > i')
+    } catch (err) {
+        console.log("Error")
+    }
     const information = await page.evaluate(() => {
         let info = []
         let arrayInformation = Array.from(document.querySelectorAll('._1xnd ._4bl9 div'));
         let category = Array.from(document.querySelectorAll('._2piu ._1xnd ._4bl9 a'));
         let payment = Array.from(document.querySelectorAll('._4bl9 i'));
+        let schedule = Array.from(document.querySelectorAll('._54nh'));
 
-        let title = document.querySelector('._64-f').innerText;
-        //let menu = Array.from(document.querySelectorAll('div a'));
-        let map = document.querySelector('._4j7v img').src;
+        let title = document.querySelector('._64-f').textContent;
+
         let logo = document.querySelector('._4jhq').src;
         info.push(category.map(cat => cat.textContent));
         info.push(arrayInformation.map(info => info.textContent))
-        info.push(title, logo, map)
+        info.push(title, logo)
+        try {
+            let map = document.querySelector('._4j7v img').src;
+            info.push(map)
+        } catch (err) {
+            info.push('')
+        }
         info.push(payment.map(pay => pay.className));
-        //info.push(menu.map(m => m.href));
+        info.push(schedule.map(sch => sch.textContent))
         return (info)
     });
-    await page.goto('https://www.facebook.com/pg/AluxPlayaDelCarmen/photos/?ref=page_internal');
-    //await page.click('#u_0_g > div:nth-child(4) > a')
+    browser.close();
+
+    //Category
+    var categories = information[0][information[0].length - 1];
+    var subcategories = information[0][information[0].length - 2];
+
+    //Information
+    var name = information[2];
+    var logo = information[3];
+
+    //Payment
+    var methodPaid = [];
+    for (let i = 0; i < information[5].length; i++) {
+        if (information[5][i] == '_3-8_ img sp_ri3k8PFTekh_1_5x sx_4cbbd5' || information[5][i] == '_3-8_ img sp_ri3k8PFTekh sx_84b1fd') {
+            methodPaid.push("American Express");
+        }
+        if (information[5][i] == '_3-8_ img sp_ri3k8PFTekh_1_5x sx_9ab5b0' || information[5][i] == '_3-8_ img sp_ri3k8PFTekh sx_1c3f5c') {
+            methodPaid.push("Visa");
+        }
+        if (information[5][i] == '_3-8_ img sp_ri3k8PFTekh_1_5x sx_cc2dd6' || information[5][i] == '_3-8_ img sp_ri3k8PFTekh sx_92ef7a') {
+            methodPaid.push("Master Card");
+        }
+    }
+
+    //Maps
+    try {
+        var longitude;
+        var latitude;
+        var direction = information[4].split("&");
+        for (let i = 0; i < direction.length; i++) {
+            if (direction[i].includes('markers=')) {
+                var dir = direction[i].split("%2C")
+                latitude = dir[0].replace('markers=', '');
+                longitude = dir[1];
+            }
+        }
+    } catch (err) {
+        latitude = null
+        longitude = null
+    }
+
+    //Address
+    var address
+    var city
+    if (latitude != null || latitude != undefined) {
+        address = information[1][0];
+        city = information[1][1];
+    } else {
+        address = ''
+        city = ''
+    }
+
+    //Call & Description
+    var call
+    var description
+    for (let i = 0; i < information[1].length; i++) {
+        var esCall = information[1][i].includes("Llamar");
+        var enCall = information[1][i].includes("Call");
+        var esDes = information[1][i].includes("Descripción");
+        var enDes = information[1][i].includes("About");
+
+        if (esCall) {
+            call = information[1][i].split("Llamar ");
+        }
+        if (enCall) {
+            call = information[1][i].split("Call ");
+        }
+        if (esDes || enDes) {
+            description = information[1][i + 1];
+        }
+
+    }
+
+    //Phone
+    try {
+        var phone = call[1];
+        var hour = schedule.split("-");
+    } catch (err) {
+        hour = ['', '']
+    }
+
+    var day = []
+    var hour = []
+    for (let i = 0; i < information[6].length; i++) {
+        let datetime = information[6][i].split(': ')
+        day.push(datetime[0])
+        hour.push(datetime[1])
+    }
+    //JSON file
+    var jsonInfo = {
+        "bussinesInformation": {
+            "name": name,
+            "logo": logo,
+            "payment": methodPaid,
+            "phone": phone,
+            "description": description
+        },
+        "ubication": {
+            "latitude": latitude,
+            "longitude": longitude,
+            "address": address,
+            "city": city
+        },
+        "categories": {
+            "categories": categories,
+            "subcategories": subcategories
+        },
+        "schedule": {
+            "hour": hour,
+            "days": day
+        }
+    }
+
+    return (jsonInfo);
+}
+
+exports.gallery = async function gallery(facebookPage) {
+    const browser = await puppeteer.launch({
+        headless: true
+    });
+    const page = await browser.newPage();
+    await page.goto(facebookPage + 'photos/?ref=page_internal');
     await page.waitForSelector('._2eea img');
     const gallery = await page.evaluate(() => {
         let image = []
@@ -44,83 +169,23 @@ const translate = require('translate');
         return (image)
 
     });
-
-    //Category
-    var categories = information[0][4];
-    var subcategories = information[0][3];
-
-    //Information
-    var name = information[2];
-    var logo = information[3];    
-
-    //Adrress
-    var address = information[1][0];
-    var city = information[1][1];
-
-    //Payment
-    var methodPaid = [];
-    for (let i = 0; i < information[5].length; i++) {
-        if(information[5][i] == '_3-8_ img sp_ri3k8PFTekh_1_5x sx_4cbbd5'){
-            methodPaid.push("American Express");
-        }
-        if(information[5][i] == '_3-8_ img sp_ri3k8PFTekh_1_5x sx_9ab5b0'){
-            methodPaid.push("Visa");
-        }
-        if(information[5][i] == '_3-8_ img sp_ri3k8PFTekh_1_5x sx_cc2dd6'){
-
-            methodPaid.push("Master Card");
-        }
-    }
-
-    //Maps
-    var longitude;
-    var latitude;
-    var direction = information[4].split("&");
-    for (let i = 0; i < direction.length; i++) {
-        if(direction[i].includes('markers=')){
-            var dir = direction[i].split("%2C")
-            latitude = dir[0].replace('markers=', '');
-            longitude = dir[1];
-        }
-    }
-    //Phone & Schedule
-    var schedule
-    var call
-    var description
-    for (let i = 0; i < information[1].length; i++) {
-        if(information[1][i].includes("Llamar")){
-            call = information[1][i].split("Llamar ");
-        }
-        if(information[1][i].includes("Cerrado ahora") || information[1][i].includes("Abierto ahora") || information[1][i].includes("Abre") || information[1][i].includes("Cierra")){
-            schedule = information[1][i+1];
-        }
-        if(information[1][i].includes("Descripción")){
-            description = information[1][i+1];
-        }
-
-    }
-    var phone = call[1];
-    var hour = schedule.split("-");
+    browser.close()
 
     for (let i = 0; i < gallery[0].length; i++) {
         if (gallery[0][i] == "https://www.facebook.com/images/photos/profile/gradient.png") {
             gallery[0].splice(i, 1)
         }
     }
-    console.log("Categories: " +categories + "\nSubcategories: " +subcategories);
-    console.log("Name:" + name + "\nLogo: " + logo);
-    console.log("Hora: " + hour[0] + " - " + hour[1]);
-    console.log("Dirección: " + address + "Ciudad: " + city);
-    console.log("Telefono: " + phone);
-    console.log("Descripcion:" + description);
-    console.log("Latitude: " + latitude + "\nLongitude: " +  longitude)
-    console.log("Metodos de Pago: " + methodPaid)
 
-    /*
-    for (let i = 0; i < gallery[0].length; i++) {
-        console.log(gallery[0][i])
+    var jsonGal = {
+        "gallery": []
     }
-    */
 
-    //browser.close();
-})();
+    for (let i = 0; i < gallery[0].length; i++) {
+        jsonGal.gallery.push({
+            'img': gallery[0][i]
+        })
+    }
+
+    return jsonGal;
+}
